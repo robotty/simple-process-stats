07abbc9fc50c90c04c91e814397c6ce0a0fb4583
Ruben Anders <ruben.anders@robotty.de>
2020-08-26 01:05:40 +0200

    get pid using libc

--- src/macos.rs
+++ src/macos.rs
@@ -2,7 +2,8 @@ use crate::{Error, ProcessStats};
 use winapi::_core::time::Duration;
 
 pub fn get_info() -> Result<ProcessStats, Error> {
-    let proc_info = darwin_libproc::task_info(std::process::id()).map_err(Error::SystemCall)?;
+    let pid = unsafe { libc::getpid() };
+    let proc_info = darwin_libproc::task_info(pid).map_err(Error::SystemCall)?;
 
     Ok(ProcessStats {
         memory_usage_bytes: proc_info.pti_resident_size,
